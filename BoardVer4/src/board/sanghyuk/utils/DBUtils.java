7a0facea387f3376b77f48e8612cac59c4b3b2b1
eofoeofo <durk011@naver.com>
2021-05-12 23:58:53 +0900

    21.05.12 JSP JDBC설정 및 VO생성

---
+++ BoardVer4/src/board/sanghyuk/utils/DBUtils.java
@@ -0,0 +1,32 @@
+package board.sanghyuk.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBUtils {
+	public static Connection getCon() throws Exception{
+		final String DB_NAME = "signup";
+		final String DRIVER = "com.mysql.cj.jdbc.Driver";
+		final String URL = "jdbc:mysql://localhost:3308/"+DB_NAME;
+		final String USER_NAME = "root";
+		final String PASSWORD = "1234";
+		
+		Class.forName(DRIVER);
+		Connection con = DriverManager.getConnection(URL,USER_NAME,PASSWORD);
+		
+		return con;
+	}
+	public static void close(Connection con, PreparedStatement ps) {
+		close(con,ps,null);
+	}
+	public static void close(Connection con, PreparedStatement ps, ResultSet rs) {
+		if(rs != null) { 
+			try {rs.close();} catch(SQLException e) { e.printStackTrace(); } // JAVA와 DB를 연결 해주는 다리 역활?
+			try {ps.close();} catch(SQLException e) { e.printStackTrace(); } 
+			try {con.close();} catch(SQLException e) { e.printStackTrace(); } // "select"문의 역활, 결과값을 받는 역활
+		}
+	}
+}
---
+++ BoardVer4/src/board/sanghyuk/utils/MyUtils.java
@@ -0,0 +1,34 @@
+package board.sanghyuk.utils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import board.sanghyuk.user.UserVO;
+
+public class MyUtils {
+	public static UserVO getLoginUser(HttpServletRequest request) {
+		if(request == null) { return null;}
+		HttpSession hs = request.getSession();
+		return (UserVO)hs.getAttribute("loginUser");
+	}
+	
+	public static int getLoginUserPk(HttpServletRequest request) {
+		return getLoginUser(request).getIuser();
+	}
+	public static int getparamInt(String key, HttpServletRequest request) {
+		String val = request.getParameter(key);
+		int intVal = parseStringToInt(val);
+		return intVal;
+	}
+	public static int parseStringToInt(String val) {
+		try {
+		return Integer.parseInt(val);
+		} catch(Exception e) {
+			return 0;
+		}
+	}
+	public static void openJsp(String fileNm, HttpServletRequest request, HttpServletRespnse response) {
+		String jsp = "WEB-INF/view/" + fileNm + ".jsp";
+		request.getRequestDispatcher(jsp).forward(request, response);
+	}
+}
